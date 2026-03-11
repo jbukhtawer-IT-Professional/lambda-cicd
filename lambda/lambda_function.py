203fe4d57ba80fa8530332466f994537aba02f10
Junaid B <juni@Junaids-MacBook-Pro.local>
2026-03-11 18:46:28 -0400

    updated errors fixed new 2026

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello updated Lambda from vscode using workflow actions github 2026 NEW')
+        'body': json.dumps('Hello updated Lambda from vscode using workflow actions github 2026')
     }
